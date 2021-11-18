ec8f7ca454223fcba5a98b6f2a566b0bb544b19d
Sarnika <nandipati.sarnika@gmail.com>
2021-11-17 22:11:56 -0500

    build

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import photo from '../assets/png/sarnika.jpeg'
 export const headerData = {
     name: 'Sarnika Nandipati',
     title: "Masters Student in Data Science & Analytics",
-    desciption:"Actively looking for Summer 2022 Internship Oppurtunities in the field of Data Science. Open for full time roles starting January 2023",
+    desciption:"Actively looking for full-time and Summer 2022 internship opportunities in the field of Data Science",
     image: photo,
     resumePdf: resume
 }
